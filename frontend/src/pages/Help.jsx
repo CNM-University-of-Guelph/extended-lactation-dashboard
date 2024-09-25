580544490a2bbd74abbfb7b512d7b138652ed103
BFieguth <bfieguth@uoguelph.ca>
2024-09-25 07:51:03 -0400

    setup all the pages with blank templates

---
+++ frontend/src/pages/Help.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+function Help() {
+    return (
+        <div>
+            <Header />
+            <h1>Help Page</h1>
+        </div>
+    );
+}
+
+export default Help;
---
+++ frontend/src/pages/Predictions.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+function Predictions() {
+    return (
+        <div>
+            <Header />
+            <h1>Predictions Page</h1>
+        </div>
+    );
+}
+
+export default Predictions;
