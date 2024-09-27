052faa5d1396915a7d012b7c139e0656407a7aa3
BFieguth <bfieguth@uoguelph.ca>
2024-09-27 14:14:27 -0400

    feat: add models to admin page

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Cow, Lactation, LactationData
 
 # Register your models here.
+admin.site.register(Cow)
+admin.site.register(Lactation)
+admin.site.register(LactationData)
