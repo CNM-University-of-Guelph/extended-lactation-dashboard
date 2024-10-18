08bd6adc55268d1835ebfd1724bb1c6bb841cdc3
BFieguth <bfieguth@uoguelph.ca>
2024-10-18 16:28:58 -0400

    feat(NotFound): update not found page with button to return to home

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,8 +1,24 @@
-function NotFound() {
-    return <div>
-        <h1>404 Not Found</h1>
-        <p>The page you are looking for doesn't exist!</p>
-    </div>
-}
+import Navbar from "../components/Navbar";
+import "../styles/NotFound.css"
 
-export default NotFound 
+function NotFound() {
+    return (
+      <div>
+        <Navbar />
+        <div className="not-found-container">
+          <h1 className="not-found-header">404 Not Found</h1>
+          <p className="not-found-message">
+            Oops! The page you are looking for doesn't exist.
+          </p>
+          <button
+            className="not-found-button"
+            onClick={() => window.location.href = "/"} // Redirect to home
+          >
+            Back to Home
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+export default NotFound;
